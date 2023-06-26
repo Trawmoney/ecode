12db1babc10b1d95918dacae295b8879c56f7644
technomage <79415352+Trawmoney@users.noreply.github.com>
2023-06-26 16:05:40 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+var codeMirrorInstance = CodeMirror.fromTextArea(document.getElementById("code"), {
+    lineNumbers: true, // Show line numbers
+    theme: "default",  // Set theme
+    mode: "javascript" // Set language mode
+});
